5af72515758fe0a11b86e6cce5d12b40f2e0b00c
LTGPanda <jessehoebe@live.nl>
2021-01-20 16:41:50 +0100

    a pure moment of bliss

--- main.py
+++ main.py
@@ -10,6 +10,28 @@ from kivy.uix.boxlayout import BoxLayout
 global ActivPlayers, ent, Sliddas
 ActivPlayers, Sliddas = [], []
 
+
+class SoundControl(BoxLayout):
+    def VolComand(Slider):
+            CUM = int(round(Slider.ids['Da_Slider'].value/2))
+
+            play.audio_set_volume(CUM)
+
+            label = self.ids['Slider_val']
+            slidy = self.ids['Da_Slider']
+            label.text = str(int(slidy.value))
+
+    def BigPP(play):
+        play.pause()
+
+    def Order66(play, Slider):
+        ActivPlayers.remove(play)
+
+        Slider.destroy()
+        kill.destroy()
+        PlayPause.destroy()
+
+        play.stop()
 class MainSetup(BoxLayout):
     def get_slider_val(self, *args):
         label = self.ids['Slider_val']
@@ -19,11 +41,17 @@ class MainSetup(BoxLayout):
 
     def Add_Sound_Control(self, *args):
         Added_Lad = SoundControl()
-        MainApp = self.ids['Main_Box']
+        Sliddas.append(Added_Lad)
+        MainApp = self.ids['Scroller']
         MainApp.add_widget(Added_Lad)
 
-class SoundControl(BoxLayout):
-    pass
+        Play = VC.Playa(self.ids['Link_input'].text)
+        label.on_touch_move(Added_Lad.VolComand(Added_Lad))
+
+
+        Added_Lad.ids['Death'].on_press(Added_Lad.Order66(Play))
+        Added_Lad.ids['PP'].on_press(Added_Lad.BigPP(Play, Sliddas))
+
  
 class SoundBoardApp(App):
     def build(self):
