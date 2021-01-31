b197cfe5032098553aaffff8a4b837a0ab597497
LTGPanda <jessehoebe@live.nl>
2021-01-31 23:32:37 +0100

    shit be getting close tho

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ Config.set('graphics', 'width', '1000')
 Config.set('graphics', 'height', '500')
 
 from kivy.uix.boxlayout import BoxLayout
-
+global LastPlayer, LastControll
 global ActivPlayers, ent, Sliddas
 ActivPlayers, Sliddas = [], []
 
-
 class SoundControl(BoxLayout):
+    def __init__(self, play, **kwargs):
+        self.Play = play
+        self.ids['Da_Slider'].on_touch_move(self.VolComand(self, self.Play.Instas))
+        self.ids['Death'].on_press(self.Order66(self.Play))
+        self.ids['PP'].on_press(self.BigPP(self.Play, Sliddas))
+        super(play, self).__init__(**kwargs)
+
     def VolComand(self, Slider, play):
             CUM = int(round(Slider.ids['Da_Slider'].value/2))
 
@@ -32,6 +38,7 @@ class SoundControl(BoxLayout):
         PlayPause.destroy()
 
         play.stop()
+
 class MainSetup(BoxLayout):
     def get_slider_val(self, *args):
         label = self.ids['Slider_val']
@@ -40,18 +47,25 @@ class MainSetup(BoxLayout):
         label.text = str(int(slidy.value))
 
     def Add_Sound_Control(self, *args):
-        Added_Lad = SoundControl()
-        Sliddas.append(Added_Lad)
         MainApp = self.ids['Scroller']
-        MainApp.add_widget(Added_Lad)
+        
 
         Play = VC.Playa(str(self.ids['Link_input'].text), None)
         Play.PlayCon()
+        Added_Lad = SoundControl(Play)
+        MainApp.add_widget(Added_Lad)
+        Sliddas.append(Added_Lad)
         ActivPlayers.append(Play)
-        Added_Lad.ids['Da_Slider'].on_touch_move(Added_Lad.VolComand(Added_Lad, Play.Instas))
-        
-        Added_Lad.ids['Death'].on_press(Added_Lad.Order66(Play))
-        Added_Lad.ids['PP'].on_press(Added_Lad.BigPP(Play, Sliddas))
+        #Event_Binder(self, Added_Lad, Play)
+        LastPlayer = Play
+        LastControll = Added_Lad
+
+        #Added_Lad.ids['Da_Slider'].on_touch_move(Added_Lad.VolComand(Added_Lad, Play.Instas))
+        #Event_Binder(Added_Lad, Play)
+        #Exception has occurred: AttributeError
+        #'NoneType' object has no attribute 'grab_current'
+        #Added_Lad.ids['Death'].on_press(Added_Lad.Order66(Play))
+        #Added_Lad.ids['PP'].on_press(Added_Lad.BigPP(Play, Sliddas))
 
  
 class SoundBoardApp(App):
